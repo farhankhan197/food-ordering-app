9724fbc0a0ae84411e072173b47711262ee75301
Farhan Khan <farhankhan.code@gmail.com>
2025-03-05 08:59:12 +0000

    text changes

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import Colors from '@/src/constants/Colors';
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
@@ -6,26 +7,24 @@ import { Text, View } from '@/src/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello world</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Daal Makhani</Text>
+      <Text style={styles.price}>Price :  ₹ 80 </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical:10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+
+  price:{
+    color:Colors.light.tint,
+    fontWeight:'bold',
+  }
 });
