9b6a022ae605d8a4ce9b01de0e30fd0f7d1f104f
Farhan Khan <farhankhan.code@gmail.com>
2025-03-05 09:33:33 +0000

    add product list component

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,40 +1,12 @@
-import { StyleSheet , Image } from 'react-native';
-import Colors from '@/src/constants/Colors';
+import {View } from 'react-native';
+import ProductListItem from '@/src/components/ProductListItem';
 import products from '@/assets/data/products';
 
-const product = products[0];
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source= {{uri : product.image}} style={styles.image}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+   <View>
+    <ProductListItem product={products[1]}/>
+    <ProductListItem product={products[5]}/>
+   </View> 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor:'white',
-    padding:10,
-    borderRadius:20,
-  },
-  image:{
-    width:'100%',
-    aspectRatio:1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical:10,
-  },
-
-  price:{
-    color:Colors.light.tint,
-    fontWeight:'bold',
-  }
-});
