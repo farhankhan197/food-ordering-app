5f93baeeb5072994703adfd8b4e4fb3d408fca4c
Farhan Khan <farhankhan.code@gmail.com>
2025-03-05 10:05:27 +0000

    add flatlist

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
-import {View } from 'react-native';
-import ProductListItem from '@/src/components/ProductListItem';
-import products from '@/assets/data/products';
+import { View, FlatList } from "react-native";
+import ProductListItem from "@/src/components/ProductListItem";
+import products from "@/assets/data/products";
 
 export default function MenuScreen() {
   return (
-   <View>
-    <ProductListItem product={products[1]}/>
-    <ProductListItem product={products[5]}/>
-   </View> 
+    <View>
+      {/* <ProductListItem product={products[1]}/>
+    <ProductListItem product={products[5]}/> */}
+      <FlatList
+        data={products}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+      />
+    </View>
   );
 }
--- FoodOrdering/src/components/ProductListItem.tsx
+++ FoodOrdering/src/components/ProductListItem.tsx
@@ -1,45 +1,52 @@
-import { StyleSheet , Image } from 'react-native';
-import Colors from '@/src/constants/Colors';
-import { Product } from '../types';
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import { StyleSheet, Image } from "react-native";
+import Colors from "@/src/constants/Colors";
+import { Product } from "../types";
+import EditScreenInfo from "@/src/components/EditScreenInfo";
+import { Text, View } from "@/src/components/Themed";
 
-const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonawn.com/food/default.png"
+const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonawn.com/food/default.png";
 
 type ProductListItemProps = {
-    product:Product;
-}
- 
-const ProductListItem = ({product}:ProductListItemProps)=>{
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-    <Image source= {{uri : product.image || defaultPizzaImage}} style={styles.image}/>
-    <Text style={styles.title}>{product.name}</Text>
-    <Text style={styles.price}>${product.price}</Text>
-  </View>
-  )
-}
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
 
-export default ProductListItem;  
+export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:'white',
-    padding:10,
-    borderRadius:20,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
-  image:{
-    width:'100%',
-    aspectRatio:1,
+  image: {
+    width: "100%",
+    aspectRatio: 1,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical:10,
+    fontWeight: "bold",
+    marginVertical: 10,
   },
 
-  price:{
-    color:Colors.light.tint,
-    fontWeight:'bold',
-  }
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
 });
